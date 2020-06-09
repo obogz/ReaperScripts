f7e8fb5bff0a4a1111d224de5d362bbc812f2c18
obogz <obogzch@gmail.com>
2020-06-10 01:41:40 +0300

    Update ReaperTonStepInsertOrModifyHeldNotesByGridCC.lua

--- ReaperTonMidiRoll/ReaperTonStepInsertOrModifyHeldNotesByGridCC.lua
+++ ReaperTonMidiRoll/ReaperTonStepInsertOrModifyHeldNotesByGridCC.lua
@@ -15,10 +15,16 @@ reaper.Undo_BeginBlock()
 
 local is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
 reaper.ShowConsoleMsg("val "..val.."\n")
-if val>0 then
-	insertOrModifyHeldNotesByGrid(1)
-elseif val<0 then    
-	insertOrModifyHeldNotesByGrid(-1)
+
+is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
+if is_new then
+	reaper.ShowConsoleMsg(name .. "\nrel: " .. rel .. "\nres: " .. res .. "\nval = " .. val .. "\n")
+	
+	if val>0 then
+		insertOrModifyHeldNotesByGrid(1)
+	elseif val<0 then    
+		insertOrModifyHeldNotesByGrid(-1)
+	end
 end
 
 reaper.Undo_EndBlock("ReaperTon Step - move Cursor CC fine By Grid Size And Alter Duration Of Held Notes", -1)
